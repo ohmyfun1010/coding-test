5db6ec718bb18fb76ee5ea28ec3bf461852c4e92
ohjiseok <ohmyfun@naver.com>
2025-11-21 23:54:46 +0900

    MergeTwoSortedLists

--- src/MergeTwoSortedLists.java
+++ src/MergeTwoSortedLists.java
@@ -1,46 +1,30 @@
 package org.src;
 
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class MergeTwoSortedLists {
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-        List<Integer> list = new ArrayList<>();
-        ListNode prevNode;
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
 
-        if(list1!=null){
-            list.add(list1.val);
-            while(list1.next != null){
-                prevNode = list1.next;
-                list.add(prevNode.val);
-                list1 = prevNode;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
             }
+            current = current.next;
         }
 
-        if(list2!=null){
-            list.add(list2.val);
-            while(list2.next != null){
-                prevNode = list2.next;
-                list.add(prevNode.val);
-                list2 = prevNode;
-            }
+        if (list1 != null) {
+            current.next = list1;
+        } else if (list2 != null) {
+            current.next = list2;
         }
 
-        Collections.sort(list, Collections.reverseOrder());
-        ListNode result = null;
-
-        if(!list.isEmpty()){
-            result = new ListNode(list.get(0));
-            for(int i = 1;i<list.size();i++){
-                result = new ListNode(list.get(i),result);
-            }
-        }
-
-        return result;
+        return dummy.next;
 
     }
 
