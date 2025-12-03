cfac41e4d6779b1e1688be3889dbe8977a675051
ohjiseok <ohmyfun@naver.com>
2025-12-03 20:39:25 +0900

    RansomNote - ascii 활용

--- src/RansomNote.java
+++ src/RansomNote.java
@@ -7,28 +7,15 @@ public class RansomNote {
 
     public boolean canConstruct(String ransomNote, String magazine) {
 
-        Map<String,Integer> ransomMap = new HashMap<>();
-        for(int i = 0;i<ransomNote.length();i++){
-            String key = ransomNote.substring(i,i+1);
-            ransomMap.put(key,ransomMap.getOrDefault(key,0)+1);
+        int[] count = new int[26];
+
+        for (char c : magazine.toCharArray()) {
+            count[c - 'a']++;
         }
 
-        Map<String,Integer> magaMap = new HashMap<>();
-        for(int i = 0;i<magazine.length();i++){
-            String key = magazine.substring(i,i+1);
-            magaMap.put(key,magaMap.getOrDefault(key,0)+1);
-        }
-
-        for(Map.Entry<String,Integer> entry : magaMap.entrySet()){
-
-            if(ransomMap.get(entry.getKey())!=null){
-                ransomMap.put(entry.getKey(),ransomMap.get(entry.getKey())-entry.getValue());
-            }
-
-        }
-
-        for(Map.Entry<String,Integer> entry : ransomMap.entrySet()){
-            if(entry.getValue()>0){
+        for (char c : ransomNote.toCharArray()) {
+            count[c - 'a']--;
+            if (count[c - 'a'] < 0) {
                 return false;
             }
         }
@@ -39,7 +26,7 @@ public class RansomNote {
 
     public static void main(String[] argc){
         RansomNote main = new RansomNote();
-        System.out.println(main.canConstruct("baawd","awdawb"));
+        System.out.println(main.canConstruct("a","b"));
     }
 
 }
