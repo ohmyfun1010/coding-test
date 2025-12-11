08991303cdfbb11decbf5a9f9ce4caaa0895be44
ohjiseok <ohmyfun@naver.com>
2025-12-11 21:14:39 +0900

    LongestCommonPrefix - 시간복잡도 개선

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -3,43 +3,23 @@ package org.src;
 public class LongestCommonPrefix {
 
     public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
 
-        StringBuilder prefix = new StringBuilder();
+        String prefix = strs[0];
 
-        int minLength = Integer.MAX_VALUE;
-        for (int i = 0; i < strs.length; i++) {
-            if (strs[i].length() < minLength) {
-                minLength = strs[i].length();
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
             }
         }
 
-        for (int i = 0; i < minLength; i++) {
-            String currentChar = "";
-            int matchCount = 0;
-
-            for (int j = 0; j < strs.length; j++) {
-                if (currentChar.isEmpty()) {
-                    currentChar = strs[j].substring(i, i + 1);
-                    matchCount++;
-                } else if (currentChar.equals(strs[j].substring(i, i + 1))) {
-                    matchCount++;
-                }
-            }
-
-            if (strs.length == matchCount) {
-                prefix.append(currentChar);
-            } else {
-                return prefix.toString();
-            }
-        }
-
-        return prefix.toString();
-
+        return prefix;
     }
 
     public static void main(String[] argc){
         LongestCommonPrefix main = new LongestCommonPrefix();
-        System.out.println(main.longestCommonPrefix(new String[]{"cir","car"}));
+        System.out.println(main.longestCommonPrefix(new String[]{"flower","flow","flight"}));
     }
 
 }
