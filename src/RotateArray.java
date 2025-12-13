07966e8757910b7987fbb1dfb97a6219fba677fa
ohjiseok <ohmyfun@naver.com>
2025-12-13 09:28:44 +0900

    RotateArray

---
+++ src/RotateArray.java
@@ -0,0 +1,29 @@
+package org.src;
+
+public class RotateArray {
+
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] argc){
+        RotateArray main = new RotateArray();
+        main.rotate(new int[]{1},1);
+    }
+
+}
