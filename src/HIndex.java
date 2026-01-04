f0ff18f7a294fb6ebe185863e3715293d4687ea3
ohjiseok <ohmyfun@naver.com>
2026-01-04 20:13:43 +0900

    HIndex

---
+++ src/HIndex.java
@@ -0,0 +1,27 @@
+package org.src;
+
+import java.util.Arrays;
+
+public class HIndex {
+
+    public int hIndex(int[] citations) {
+
+        int max = 0;
+        Arrays.sort(citations);
+        int n = citations.length;
+
+        for (int i = 0; i < n; i++) {
+            int h = Math.min(citations[i], n - i);
+            max = Math.max(max, h);
+        }
+
+        return max;
+
+    }
+
+    public static void main(String[] argc){
+        HIndex main = new HIndex();
+        System.out.println(main.hIndex(new int[]{5,5,5,5}));
+    }
+
+}
